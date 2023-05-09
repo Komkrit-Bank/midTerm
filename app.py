27ba906c2ddcc92bae2caf470d039096132020f2
Komkrit-Bank <133026403+Komkrit-Bank@users.noreply.github.com>
2023-05-09 23:27:46 +0700

    update_index
    add navbar and carousel

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-app.run()
+@app.route('/product/<id>')
+def product(id):
+    return render_template('product.html', id= id)
+
+app.run(debug= True)
