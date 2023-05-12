9f4d7ccc60716b98eda86ff55c94df1c162104fd
Komkrit-Bank <133026403+Komkrit-Bank@users.noreply.github.com>
2023-05-13 02:28:27 +0700

    create test of fetch and js

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    test = 'test'
+    review = request.form.get('review_select')
+    return render_template('index.html', review= review, test= test)
 
 @app.route('/product')
 def product():
     return render_template('product.html')
+
+@app.route('/fetchtestpage', methods= ['GET', 'POST'])
+def fetchtestpage():
+    return render_template('fetchtestpage.html')
+
+@app.route('/fetchtest', methods= ['GET', 'POST'])
+def fetchtest():
+    return {'some text': 'it was a success!'}
 
 app.run(debug= True)
