9f1673606ea9988bcb4de6d953369a0e4fab0a94
Rokoshan <133026403+Komkrit-Bank@users.noreply.github.com>
2023-05-15 13:46:39 +0700

    update database

--- app.py
+++ app.py
@@ -5,21 +5,43 @@ import sqlite3
 app = Flask(__name__)
 
 conn = sqlite3.connect('product.db')
-cur = conn.cursor()
 
-def extract_data(data_df):
-    data_key = [row[1][0] for row in data_df.iterrows()]
-    data_col = data_df.columns.to_list()[1:]
-    data_body = []
+# def extract_data(data_df):
+#     data_key = [row[1][0] for row in data_df.iterrows()]
+#     data_col = data_df.columns.to_list()[1:]
+#     data_body = []
 
-    for row in data_df.iterrows():
-        data_body.append(dict(zip(data_col, row[1][1:])))
+#     for row in data_df.iterrows():
+#         data_body.append(dict(zip(data_col, row[1][1:])))
 
-    complete_data = dict(zip(data_key, data_body))
-    return complete_data
+#     complete_data = dict(zip(data_key, data_body))
+#     return complete_data
+
+def update_sql(type, conn, task, id):
+    if type == 'product':
+        sql = f''' UPDATE product
+                    SET product_name = ? ,
+                        product_price = ? ,
+                        product_cat = ?,
+                        product_review = ?,
+                        color = ?,
+                        material = ?,
+                        offer = ?,
+                        product_detail = ?
+                    WHERE product_id = {id}'''
+    else:
+        return
+    
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
 
 product_df = pd.read_csv(r'C:\Users\HP\Documents\GitHub\midTerm.github.io\src\productData.csv', encoding= 'utf8')
-print(extract_data(product_df))
+rows = [tuple(row[1].to_list()) for row in product_df.iterrows()]
+
+for row in rows:
+    update_sql('product', conn, row[1:], row[0])
+# Use executemany() to insert multiple records at a time
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
